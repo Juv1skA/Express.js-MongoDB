066c33702667cad051de63469d5c47bc6840d20a
Jouni Juvonen <ejoju004@edu.xamk.fi>
2024-02-05 11:32:28 +0200

    implimented cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,17 @@
 // imports express
 const express = require('express');
 
+//imports cors
+const cors = require('cors');
+
+// use CORS middleware
+app.use(cors());
+
 // create instance of express
 const app = express();
 
-// define server port
-const PORT = 3000;
+// define server port and middleware
+const PORT = process.env.PORT || 3000;
 
 //define route for GET to root URL
 app.get('/', (req, res) => {
